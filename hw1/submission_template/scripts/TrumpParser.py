98cca7da1b10933efb3c3d1bc679ef7c2be4e6f5
jonayed-i <71873900+jonayed-i@users.noreply.github.com>
2021-09-26 14:07:47 -0400

    update hw3

--- hw1/submission_template/scripts/TrumpParser.py
+++ hw1/submission_template/scripts/TrumpParser.py
@@ -9,7 +9,32 @@ print(df.describe())
 
 
 def containsTrump(content):
-    return True
+
+    if(re.search("[^a-zA-Z]Trump[^a-zA-Z]|^Trump[^a-zA-Z]]|[^a-zA-Z]Trump\Z",content)):
+        return True
+    else:
+        return False
 
 
-df["isQuestion"] = df['content'].apply(isQuestion)
+df["trump_mention"] = df['content'].apply(containsTrump)
+df = df[['tweet_id','publish_date','content','trump_mention']]
+print(df.describe())
+
+
+trump_mentions = df.trump_mention[df.trump_mention==True].count()
+notTrump_mentions = df.trump_mention[df.trump_mention==False].count()
+
+fraction = (trump_mentions)/(trump_mentions+notTrump_mentions)
+rounded = round(fraction,3)
+
+results = pd.DataFrame(columns =['result', 'value'])
+#new_row = 'frac-trump-mentions' + rounded
+#results.append({'result':'frac-trump-mentions','value':rounded},ignore_index=True)
+results.loc[len(results.index)] = ['frac-trump-mentions',rounded]
+print(results.head())
+print("hello")
+
+
+
+df.to_csv (r'C:\Users\joisl\Documents\McGill\Comp 598\hw1\submission_template\data\dataset.tsv', index = False, header=True, sep = '\t')
+results.to_csv (r'C:\Users\joisl\Documents\McGill\Comp 598\hw1\submission_template\data\results.tsv', index = False, header=True, sep = '\t')
--- hw1/submission_template/scripts/scratchpad.py
+++ hw1/submission_template/scripts/scratchpad.py
@@ -1,13 +1,19 @@
 import re
+import pandas as pd
 
 
+df = pd.read_csv(r"C:\Users\joisl\Documents\McGill\Comp 598\hw1\submission_template\data\lang_dataframe.csv")
+def containsTrump(content):
 
-def isQuestion(content):
-    if re.search(r"\?",content):
+    if(re.search("Trump",content)):
         return True
     else:
         return False
+df["trump_mention"] = df['content'].apply(containsTrump)
+trump_mentions = df.trump_mention[df.trump_mention==True].count()
+notTrump_mentions = df.trump_mention[df.trump_mention==False].count()
 
-print(isQuestion("daddygang"))
+fraction = (trump_mentions)/(trump_mentions+notTrump_mentions)
+rounded = round(fraction,3)
 
-print(re.search("\?","bryg?"))
+print(rounded)
